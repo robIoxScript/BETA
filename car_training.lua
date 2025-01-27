ad19f7c6a72cde1300798a18865ca4430253756f
robIoxScript <cypruxy@gmail.com>
2025-01-27 22:05:38 +0100

    Update car_training.lua

--- car_training.lua
+++ car_training.lua
@@ -1,46 +1,90 @@
-local ArrayField = loadstring(game:HttpGet('https://raw.githubusercontent.com/UI-Interface/ArrayField/main/Source.lua'))()
-getgenv().SecureMode = true
-    
-local Window = ArrayField:CreateWindow({
-    Name = "Symbiote HUB",
-    LoadingTitle = "Symbiote Project",
-    LoadingSubtitle = "by Xxarda_lepro67xX",
-    ConfigurationSaving = {
-       Enabled = true,
-       FolderName = "Symbiote FOLDER", -- Create a custom folder for your hub/game
-       FileName = "Symbiote HUB"
-    },
-    Discord = {
-       Enabled = false,
-       Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-    },
-    KeySystem = true, -- Set this to true to use our key system
-    KeySettings = {
-       Title = "Register",
-       Subtitle = "Key System",
-       Note = "The Key Only My Friend Know it .",
-       FileName = "Today.Key", -- It is recommended to use something unique as other scripts using ArrayField may overwrite your key file
-       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like ArrayField to get the key from
-       Actions = {
-             [1] = {
-                 Text = 'Click here to copy the key link <--',
-                 OnPress = function()
-                     print('Pressed')
-                 end,
-                 }
-             },
-       Key = {"123mno"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-    }
- })
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
- locallocal Section = Tab:CreateSection("Homme",false) -- The 2nd argument is to tell if its only a Title and doesnt contain element
+local Window = Rayfield:CreateWindow({
+   Name = "Symbiote Hub",
+   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Rayfield Interface Suite",
+   LoadingSubtitle = "by Sirius",
+   Theme = "Serenity", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
 
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Big Hub"
+   },
+
+   Discord = {
+      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Untitled",
+      Subtitle = "Key System",
+      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
+      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
+
+local TabMain = Window:CreateTab("Main", 4483362458) -- Title, Image
+local TabCredit = Window:CreateTab("Credit", 4483362458) -- Title, Image
 
 
+local Label = TabCredit:CreateLabel("Owner is 'Cypruxy'", 4483362458, Color3.fromRGB(255, 255, 255), false) -- Title, Icon, Color, IgnoreTheme
+local Button = TabMain:CreateButton({
+   Name = "Auto Race",
+   Callback = function()
+      local args = {
+         [1] = "Auto Race",
+         [2] = true
+     }
+     
+     game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Set Stat"):FireServer(unpack(args))
+   end,
+})
+
+local Button = TabMain:CreateButton({
+   Name = "Stop Auto Race",
+   Callback = function()
+      local args = {
+         [1] = "Auto Race",
+         [2] = false
+     }
+     
+     game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Set Stat"):FireServer(unpack(args))
+   end,
+})
+local Button = TabMain:CreateButton({
+   Name = "TP to Spawn",
+   Callback = function()
+      local args = {
+         [1] = 1
+     }
+     
+     game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("TP"):InvokeServer(unpack(args))
+   end,
+})
+local Button = TabMain:CreateButton({
+   Name = "Auto Merchant Event (20M)",
+   Callback = function()
+      while wait(1) do
+      (local args = {
+         [1] = 1
+     }
+     
+     game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Event Merchant"):InvokeServer(unpack(args)))
+   
+   end,
+})
 
 
 
-ArrayField:LoadConfiguration()
+Rayfield:LoadConfiguration()
